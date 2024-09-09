c5fb7eca751ff6cc6a6ba07a396031537016107b
eshan-sharma <43044334+Eshan-Sharma@users.noreply.github.com>
2024-09-09 17:11:47 +0530

    added tabs

--- app/index.tsx
+++ app/index.tsx
@@ -2,6 +2,7 @@ import { type PropsWithChildren } from "react";
 import { Text, View } from "react-native";
 import { StatusBar } from "expo-status-bar";
 import React from "react";
+import { Link } from "expo-router";
 /**
  * This file is web-only and used to configure the root HTML for every web page during static rendering.
  * The contents of this function only run in Node.js environments and do not have access to the DOM or browser APIs.
@@ -14,6 +15,7 @@ export default function Root({ children }: PropsWithChildren) {
           DollarPe.
         </Text>
         <StatusBar style="auto" />
+        <Link href={"./home"}>Go to home</Link>
       </View>
     </>
   );
