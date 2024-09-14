9421cc99e17ae1473a7ef7781b7a823ec3cec212
eshan-sharma <43044334+Eshan-Sharma@users.noreply.github.com>
2024-09-14 22:57:27 +0530

    onboarding screen

--- app/index.tsx
+++ app/index.tsx
@@ -1,22 +1,53 @@
 import { type PropsWithChildren } from "react";
-import { Text, View } from "react-native";
-import { StatusBar } from "expo-status-bar";
+import { ScrollView, Text, View, Image } from "react-native";
 import React from "react";
-import { Link } from "expo-router";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { images } from "../constants";
+import CustomButton from "@/components/CustomButton";
+import { StatusBar } from "expo-status-bar";
+import { Redirect, router } from "expo-router";
 /**
  * This file is web-only and used to configure the root HTML for every web page during static rendering.
  * The contents of this function only run in Node.js environments and do not have access to the DOM or browser APIs.
  */
 export default function Root({ children }: PropsWithChildren) {
   return (
-    <>
-      <View className="flex-1 items-center justify-center bg-white">
-        <Text className="text-3xl font-black pl-3 md:pl-0 lg:pl-0">
-          DollarPe.
-        </Text>
-        <StatusBar style="auto" />
-        <Link href={"./home"}>Go to home</Link>
-      </View>
-    </>
+    <SafeAreaView className="bg-primary h-full">
+      <ScrollView contentContainerStyle={{ height: "100%" }}>
+        <View className="w-full justify-center items-center min-h-[85vh] px-4">
+          <Image
+            source={images.logo}
+            className="w-[130px] h-[84px]"
+            resizeMode="contain"
+          ></Image>
+          <Image
+            source={images.cards}
+            className="max-w-[380px] w-full h-[300px]"
+            resizeMode="contain"
+          ></Image>
+          <View className="relative mt-5">
+            <Text className="text-3xl text-white font-bold text-center">
+              Discover Endless Possibilities with{" "}
+              <Text className="text-secondary-200">Aora</Text>
+            </Text>
+            <Image
+              source={images.path}
+              className="w-[136px] h-[15px] absolute -bottom-2 -right-8"
+              resizeMode="contain"
+            ></Image>
+          </View>
+          <Text className="text-gray-100 mt-7 text-center text-sm font-pregular">
+            Where creativity meets innovation" embark on a journey of limitless
+            exploration with Aora
+          </Text>
+          <CustomButton
+            title="Continue with Email"
+            handlePress={() => router.push("/sign-in")}
+            containerStyle="w-full mt-7"
+          ></CustomButton>
+        </View>
+      </ScrollView>
+      <StatusBar backgroundColor="#161622" style="light"></StatusBar>
+    </SafeAreaView>
   );
 }
